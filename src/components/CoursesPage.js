aca17414a4124566c0dfd41d3cfaff5f988fc5a2
Eberechi Ogunedo <eberechi.ogunedo@ceridian.com>
2019-10-21 11:14:11 -0400

    Class vs fucntions - using hooks with functions

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,16 +1,14 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then(courses => {
-      this.setState({ courses: courses });
-    });
-  }
+  useEffect(() => {
+    getCourses().then(returnedData => setCourses(returnedData));
+  }, []);
+
   /* row can be extracted and called (with  this.renderRow) in the map
   renderRow(course) {
     return (
@@ -22,32 +20,32 @@ class CoursesPage extends React.Component {
     );
   }
 */
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map(course => {
-              return (
-                <tr key={course.id}>
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
+
 export default CoursesPage;
